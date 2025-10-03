ae66e12f158ddf5c72323b5c57c4cd85c5c05194
Matheus Carvalho <matheus_psilveira@hotmail.com>
2025-10-03 19:56:01 -0300

    xadrez nivel novato

--- xadrez.c
+++ xadrez.c
@@ -1,14 +1,28 @@
 #include <stdio.h>;
 
 int main () {
-    int numero, i;
+    int torre, bispo = 1, rainha = 1;
     
-    printf("Escolha um número: ");
-    scanf("%i", &numero);
-
-    for (i = 1; i <= 10; i++) {
-        printf("%i x %i = %i\n",  i, numero, numero * i);
+    
+    printf("Torre: \n");
+    for (torre = 1; torre <= 5; torre++ ) {
+        printf("Direita\n");
     }
+    printf("-------------------\n");
+    
+    printf("Bispo: \n");
+    while (bispo <= 5) {
+        printf("Cima\n");
+        printf("Direita\n");
+        bispo++;
+    }
+    printf("-------------------\n");
+    
+    printf("Rainha: \n");
+    do {
+        printf("Esquerda\n");
+        rainha++;
+    } while (rainha <= 8);
 
     return 0;
-}
+}   
