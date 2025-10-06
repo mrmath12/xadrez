cdc1a5ff3c23b48329aacf506610ff13ea750b2d
Matheus Carvalho <matheus_psilveira@hotmail.com>
2025-10-06 19:45:40 -0300

    nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,16 @@
 #include <stdio.h>;
 
 int main () {
-    int torre, bispo = 1, rainha = 1;
+    int torre = 1, bispo = 1, rainha = 1, cavalo = 1;
     
-    
+    // Movimento da Torre
     printf("Torre: \n");
-    for (torre = 1; torre <= 5; torre++ ) {
+    for (torre; torre <= 5; torre++ ) {
         printf("Direita\n");
     }
     printf("-------------------\n");
     
+    // Movimento do Bispo
     printf("Bispo: \n");
     while (bispo <= 5) {
         printf("Cima\n");
@@ -18,11 +19,31 @@ int main () {
     }
     printf("-------------------\n");
     
+    // Movimento da Rainha
     printf("Rainha: \n");
     do {
         printf("Esquerda\n");
         rainha++;
     } while (rainha <= 8);
+    printf("-------------------\n");
+    
+    // Movimento do Cavalo
+    printf("Cavalo: \n");
+    for (int i = 1; i <= 1; i++) {
+
+        // Movimento pra baixo
+        do {
+            
+            printf("Baixo\n");
+            cavalo++;
+            
+        } while ( cavalo == 2 );
+
+        // Movimento pra esquerda
+        printf("Esquerda\n");
+        
+        }            
+    printf("-------------------\n");
 
     return 0;
 }   
