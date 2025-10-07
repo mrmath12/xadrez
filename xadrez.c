fb4cde1ea03f2e4ea49b7e21936aed97c0846508
Matheus Carvalho <matheus_psilveira@hotmail.com>
2025-10-07 19:09:36 -0300

    desafio nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -1,49 +1,83 @@
 #include <stdio.h>;
 
+void movimentoTorre(int casas) {
+    if (casas > 0 ) {
+        printf("Direita\n");
+        movimentoTorre(casas - 1);
+    }
+}
+
+void movimentoBispo(int casas) {
+    if (casas > 0 ) {
+        printf("Cima\n");
+        printf("Direita\n");
+        movimentoBispo(casas - 1);
+    }
+}
+
+void movimentoRainha(int casas) {
+    if (casas > 0 ) {
+         printf("Esquerda\n");
+        movimentoRainha(casas - 1);
+    }
+}
+
+
 int main () {
     int torre = 1, bispo = 1, rainha = 1, cavalo = 1;
     
     // Movimento da Torre
-    printf("Torre: \n");
+    printf("TORRE: \n");
+    movimentoTorre(5);
+    /*
     for (torre; torre <= 5; torre++ ) {
         printf("Direita\n");
     }
-    printf("-------------------\n");
+    */
+    printf("-------------------\n\n");
     
     // Movimento do Bispo
-    printf("Bispo: \n");
+    printf("BISPO: \n");
+    movimentoBispo(5);
+    /*
     while (bispo <= 5) {
         printf("Cima\n");
         printf("Direita\n");
         bispo++;
     }
-    printf("-------------------\n");
+    */
+    printf("-------------------\n\n");
     
     // Movimento da Rainha
-    printf("Rainha: \n");
+    printf("DAMA: \n");
+    movimentoRainha(8);
+    /*
     do {
         printf("Esquerda\n");
         rainha++;
     } while (rainha <= 8);
-    printf("-------------------\n");
+    */
+    printf("-------------------\n\n");
     
     // Movimento do Cavalo
-    printf("Cavalo: \n");
+    printf("CAVALO: \n");
+
     for (int i = 1; i <= 1; i++) {
-
-        // Movimento pra baixo
-        do {
-            
-            printf("Baixo\n");
-            cavalo++;
-            
-        } while ( cavalo == 2 );
-
-        // Movimento pra esquerda
-        printf("Esquerda\n");
         
-        }            
-    printf("-------------------\n");
+    // Movimento pra baixo
+    do {
+        
+    printf("Cima\n");
+    cavalo++;
+    
+    } while ( cavalo == 2 );
+
+    // Movimento pra esquerda
+    printf("Direita\n");
+
+    }            
+ 
+    printf("-------------------\n\n");
 
     return 0;
 }   
